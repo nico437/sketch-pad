f26bdd093c1f9a2a18f4e8b7fb5ee90b58fbd9b3
“Nicolas <nicolas.e.bron@gmail.com>
2022-08-13 10:36:02 -0400

    added blue square

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 let board = document.querySelector('.board')
 board.style.gridTempalteColumns = 'repeat(16, 1fr)'
 board.style.gridTempalteRows = 'repeat(16, 1fr)'
+
+for(let i = 0; i<256; i++) {
+    let square = document.createElement('div')
+    square.style.backgroundColor = 'blue'
+    board.insertAdjacentElement('beforeend' , square);
+}

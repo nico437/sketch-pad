40f9ccb875373f0c387d0f041f921fd6f4bff5ef
“Nicolas <nicolas.e.bron@gmail.com>
2022-08-13 09:43:58 -0400

    removed old js code

--- script.js
+++
@@ -1,8 +0,0 @@
-document.body.onload = addElement;
-function addElement () {
-    const newDiv = document.createElement("div");
-    const newContent = document.createTextNode("Hi there and greetings!");
-    newDiv.appendChild(newContent);
-    const currentDiv = document.getElementById("div1");
-    document.body.insertBefore(newDiv, currentDiv);
-  }

4c8159eb0b385fb2ec236f4d1585e36697d254d3
“Nicolas <nicolas.e.bron@gmail.com>
2022-08-13 10:41:35 -0400

    added size to ensure it stays as a square

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function populateBoard() {
+function populateBoard(size) {
     let board = document.querySelector('.board')
-board.style.gridTempalteColumns = 'repeat(16, 1fr)'
-board.style.gridTempalteRows = 'repeat(16, 1fr)'
+board.style.gridTempalteColumns = `repeat(${size}, 1fr)`;
+board.style.gridTempalteRows = `repeat(${size}, 1fr)`;
 
 for(let i = 0; i<256; i++) {
-    let square = document.createElement('div')
-    square.style.backgroundColor = 'blue'
+    let square = document.createElement('div');
+    square.style.backgroundColor = 'blue';
     board.insertAdjacentElement('beforeend' , square);
 }  
 }
